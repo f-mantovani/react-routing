63b5cf531e87bcb21a9066284e36bd22311948d7
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-09-12 19:32:01 +0200

    Browser setup, routes, route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css'
 import Navbar from './components/Navbar.jsx'
 import Home from './components/Home.jsx'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
 	return (
 		<div className='App'>
 			<Navbar />
-			<Home />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/projects' element={<h1> Projects </h1>} />
+				<Route path='*' element={<h1>Error Page</h1>} />
+			</Routes>
 		</div>
 	)
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
+import { Link, NavLink } from 'react-router-dom'
+
 const Navbar = () => {
+	const handleClassName = ({ isActive }) => (isActive ? 'selected' : '')
 	return (
 		<div className='nav'>
-			<h2>React Routing</h2>
+			<h2>
+				<Link to='/'>React Routing</Link>
+			</h2>
 			<div className='nav-menu'>
-				<span>Projects</span>
-				<span>About</span>
+				<NavLink to='/projects'>Projects</NavLink>
+				<NavLink to='/about'>About</NavLink>
 			</div>
 		</div>
 	)
